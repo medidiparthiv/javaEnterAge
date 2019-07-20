ec87a2104d37a6c6d84b595388dbf8e31e3cd7fb
medidiparthiv <53120768+medidiparthiv@users.noreply.github.com>
2019-07-20 11:40:21 -0400

    Add files via upload

---
+++ EnterAge.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner x = new Scanner(System.in);
+	    System.out.print("Please Enter Your Age : ");
+	    Integer y = x.nextInt();
+		System.out.println("You Entered Your Age As : " + y);
+	}
+}
